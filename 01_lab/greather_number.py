374e027a5d028df934b4dbf6722991a3e1280fb0
Yozlen Aliev <yozlenaliev@gmail.com>
2025-06-03 14:34:16 +0300

    Added file greather_number.py

---
+++ 01_lab/greather_number.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+result = 0
+
+if first_number >= second_number:
+    result = first_number
+else:
+    result = second_number
+
+print(result)
